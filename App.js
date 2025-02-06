5c228ff99ac49d1d7b8138cdbc8b8af8644bf333
23048180-yongkeong <23048180@myrp.edu.sg>
2025-02-06 15:49:54 +0800

    Completed Exercise 3

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, StyleSheet,  } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [{x, y, z}, setData] = useState({x: 0, y: 0, z: 0});
+  const [shake, setShake] = useState();
+  const [text, setText] = useState(false);
+
+  async function playShake() {
+     const file = require('./shake.wav');
+     const {sound} = await Audio.Sound.createAsync(file);
+     setShake(sound);
+     setText(true);
+     sound.setOnPlaybackStatusUpdate((status) => {
+       if (status.didJustFinish) {
+         setText(false);
+       }
+     });
+     await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return shake ? () => {
+      console.log('Unloading Sound');
+      shake.unloadAsync();
+    }
+    : undefined;
+  }, [shake]);
+
+  useEffect(() => {
+    {x + y + z > 2 && playShake()}
+  }, [x + y + z]);
+
+  useEffect(() => {
+    Accelerometer.setUpdateInterval(100);
+    const subscription = Accelerometer.addListener(setData);
+    return () =>
+      subscription.remove();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{text === true && "SHAKE"}</Text>
     </View>
   );
 }
@@ -13,8 +49,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 100,
   },
 });
